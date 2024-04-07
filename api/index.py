8a156f5d560d17afc7d8f8559d9b11a970a6a360
Ömer <74078118+yennawigh@users.noreply.github.com>
2024-04-07 05:12:13 +0300

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,11 +1,90 @@
-from flask import Flask
+from flask import Flask, jsonify
+import requests
+from bs4 import BeautifulSoup
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return 'Hello, World!'
+channels = []
 
-@app.route('/about')
-def about():
-    return 'About'
+def getCharacters():
+    
+    with requests.Session() as se:
+        se.headers = {
+            "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36 Edg/121.0.0.0",
+            "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
+            "Accept-Encoding": "gzip, deflate, br",
+            "Accept-Language": "en-US,en;q=0.9"
+        }
+
+        characters = []
+        charactersURL = se.get('https://vorp.fandom.com/tr/wiki/Karakterler')
+        soup = BeautifulSoup(charactersURL.content, 'html.parser')
+
+        for i in range(22):
+            characterPageMain = f"gallery-{i}"
+            characterPageContent = soup.find('div', id=characterPageMain)
+
+            if characterPageContent:
+                charactersContent = characterPageContent.find_all('a')
+                for character in charactersContent:
+                    character_name = character.text.strip()
+                    if character_name:
+                        characters.append(character_name)
+        return characters
+
+def getCharacterDetail(name):
+    with requests.Session() as se:
+        se.headers = {
+            "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36 Edg/121.0.0.0",
+            "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
+            "Accept-Encoding": "gzip, deflate, br",
+            "Accept-Language": "en-US,en;q=0.9"
+        }
+
+    editedName2 = "FandomPP-" + name.replace(' ', '_').split("_")[0]
+    editedName = name.replace(' ', '_').split("_")[0] + name.replace(' ', '_').split("_")[1][0]
+    character_page = se.get(f"https://vorp.fandom.com/tr/wiki/{name.replace(' ', '_')}")
+    character_soup = BeautifulSoup(character_page.content, 'html.parser')
+    features_data = character_soup.find('aside')
+    pTag_data = character_soup.find_all('p')
+
+    channel_data = character_soup.find('a', class_='external free')
+    
+    if channel_data:
+        channel_url = channel_data['href']
+        if 'twitch.tv' in channel_url and 'team/vorp' not in channel_url:
+            channelName = channel_url.replace('https://www.twitch.tv/', '').replace('http://www.twitch.tv/', '').replace('https://twitch.tv/', '').replace('http://twitch.tv/', '')
+            channel = channelName.lower()
+
+    avatar = character_soup.find('img', class_="pi-image-thumbnail")
+    if avatar is None:
+        editedName = '3.0'  # editedName değeri 3.0 olarak güncelleniyor
+        avatar = character_soup.find('img', class_={'alt': editedName})
+        if avatar is None:
+            avatar = character_soup.find('img', attrs={'alt': editedName2})
+
+    if avatar is not None:
+        avatar = avatar['src']
+    else:
+        avatar =  character_soup.find('img', class_="pi-image-thumbnail")['src']
+
+    bio = max(pTag_data, key=lambda p: len(p.get_text())).text.strip()
+    features = {}
+    for section in features_data.find_all('section'):
+        for item in section.find_all('div', class_='pi-item'):
+            label = item.find('h3', class_='pi-data-label').text.strip()
+            value = item.find('div', class_='pi-data-value').text.strip()
+            features[label] = value
+    return {'avatar': avatar, 'features': features,'bio': bio, 'channel': channel}
+
+
+@app.route('/characters', methods=['GET'])
+def list_characters():
+    return getCharacters()
+
+@app.route('/characters/<name>', methods=['GET'])
+def list_details(name):
+    return getCharacterDetail(name)
+
+if __name__ == '__main__':
+    app.run()
