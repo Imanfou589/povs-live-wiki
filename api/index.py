e7e53d83587e7c98c38c5f24906b9b17fc5de32e
Ömer <74078118+yennawigh@users.noreply.github.com>
2024-04-07 06:01:05 +0300

    Update index.py

--- api/index.py
+++ api/index.py
@@ -4,31 +4,6 @@ import requests
 
 app = Flask(__name__)
 
-def getCharacters():
-    with requests.Session() as se:
-        se.headers = {
-            "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36 Edg/121.0.0.0",
-            "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
-            "Accept-Encoding": "gzip, deflate, br",
-            "Accept-Language": "en-US,en;q=0.9"
-        }
-
-        characters = []
-        charactersURL = se.get('https://vorp.fandom.com/tr/wiki/Karakterler')
-        soup = BeautifulSoup(charactersURL.content, 'html.parser')
-
-        for i in range(22):
-            characterPageMain = f"gallery-{i}"
-            characterPageContent = soup.find('div', id=characterPageMain)
-
-            if characterPageContent:
-                charactersContent = characterPageContent.find_all('a')
-                for character in charactersContent:
-                    character_name = character.text.strip()
-                    if character_name:
-                        characters.append(character_name)
-        return characters
-
 def getCharacterDetail(name):
     with requests.Session() as se:
         se.headers = {
@@ -45,6 +20,8 @@ def getCharacterDetail(name):
     features_data = character_soup.find('aside')
     pTag_data = character_soup.find_all('p')
 
+    channel = None  # Initialize channel variable
+
     channel_data = character_soup.find('a', class_='external free')
     
     if channel_data:
@@ -52,6 +29,7 @@ def getCharacterDetail(name):
         if 'twitch.tv' in channel_url and 'team/vorp' not in channel_url:
             channelName = channel_url.replace('https://www.twitch.tv/', '').replace('http://www.twitch.tv/', '').replace('https://twitch.tv/', '').replace('http://twitch.tv/', '')
             channel = channelName.lower()
+
     bio = ""
     if pTag_data:
         bio = max(pTag_data, key=lambda p: len(p.get_text())).text.strip()
