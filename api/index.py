ac1f8b654310c2889a16b13a05143c648a748f6a
Ömer <74078118+yennawigh@users.noreply.github.com>
2024-04-08 04:25:51 +0300

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,8 +1,10 @@
 from flask import Flask, jsonify
 from bs4 import BeautifulSoup
+from flask_cors import CORS
 import requests
 
 app = Flask(__name__)
+CORS(app)
 
 def getCharacters():
     with requests.Session() as se:
